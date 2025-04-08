423512030498d37c5a8074a8635dea8031587ddc
Nathanaël Houn <contact@nathanaelhoun.fr>
2025-04-08 13:04:34 +0200

    feat: open grafana since last run date

--- run.sh
+++ run.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
-set -eux
+export PS4='\n\[\e[36m\]+ \[\e[m\]'
+set -o errexit -o nounset -o xtrace
 
 docker compose up --detach
-
-xdg-open "http://localhost:3000/d/SDksirRWz-new/dmarc-reports"
+ trap "docker compose down" EXIT
 
 docker compose logs --follow parsedmarc
+
+# Tous les rapports ont été consommés, on ouvre Grafana
+xdg-open "http://localhost:3000/d/SDksirRWz-new/dmarc-reports?from=$(cat .last-run-date)T00:00:00.000Z"
+
+date --iso-8601=date > .last-run-date
+
+# Laisser le script tourner tant qu’on ne l’arrête pas manuellement
+read -r -d '' _ </dev/tty
